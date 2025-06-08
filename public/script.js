7fa4946c9c68a3096d5c3a5c4a641d4db78b3531
MayureshMadavD <mayuresh.madav@gokwik.co>
2025-06-08 23:06:17 +0530

    ss

--- public/script.js
+++ public/script.js
@@ -1,8 +1,18 @@
 function toggleStrikeThrough(checkbox) {
-    const textSpan = checkbox.nextElementSibling.querySelector('#todo-text');
-    if (checkbox.checked) {
-      textSpan.classList.add('checked');
-    } else {
-      textSpan.classList.remove('checked');
-    }
+  const textSpan = checkbox.nextElementSibling.querySelector("#todo-text");
+  if (checkbox.checked) {
+    textSpan.classList.add("checked");
+  } else {
+    textSpan.classList.remove("checked");
   }
+}
+
+const handleEditToogle = (event, i) => {
+  event.preventDefault();
+  const listOfTodo = document.querySelectorAll("li #todo-text");
+  const targetedTodoList = listOfTodo[i];
+  console.log(targetedTodoList)
+  if (targetedTodoList) {
+    targetedTodoList.classList.add("hide");
+  }
+};
