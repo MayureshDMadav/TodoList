6ac8c6dfd241f3ee933c6101dc603772476cb972
MayureshMadavD <mayuresh.madav@gokwik.co>
2025-06-09 23:29:36 +0530

    ss

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
+const database = require("./modal/connect");
 
 const app = express();
-
-let todoListArray = [];
 
 app.use(express.static("public"));
 
@@ -10,29 +9,56 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index", { list: todoListArray });
+app.get("/", async (req, res) => {
+  const todoList = await database.find();
+  const response = await todoList;
+  res.render("index", { list: response });
 });
 
-app.post("/", (req, res) => {
-  const enteredText = req.body["todo-entry"];
-  if (enteredText?.length) {
-    todoListArray.push(enteredText);
-    res.redirect("/");
+app.post("/", async (req, res) => {
+  try {
+    const enteredText = req.body["todo-entry"];
+    const priority = req.body["priority"];
+    const createNewEntry = await database.create({
+      title: enteredText,
+      priority: priority,
+    });
+    if (createNewEntry) {
+      res.redirect("/");
+    }
+  } catch (e) {
+    console.log(e);
   }
 });
 
-app.post("/editTodo", (req, res) => {
-  const todoid = req.body["todo_id"];
-  const updatedText = req.body["edit_input"];
-  todoListArray[todoid] = updatedText;
-  res.redirect("/");
+app.post("/editTodo", async (req, res) => {
+  try {
+    const todoid = req.body["todo_id"];
+    const updatedText = req.body["edit_input"];
+    const priority = req.body["priority"];
+    const response = await database.findByIdAndUpdate(todoid, {
+      title: updatedText,
+      priority: priority,
+    });
+
+    if (response) {
+      res.redirect("/");
+    }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
-app.post("/delete", (req, res) => {
-  const dataToDelete = req.body["todo_id"];
-  todoListArray.splice(dataToDelete, 1);
-  res.redirect("/");
+app.post("/delete", async (req, res) => {
+  try {
+    const dataToDelete = req.body["todo_id"];
+    const response = await database.findByIdAndDelete(dataToDelete);
+    if (response) {
+      res.redirect("/");
+    }
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 app.listen(3000, () => {
