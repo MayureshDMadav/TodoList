10670512caa07199e611cf6fdbc3f1892f7a79d7
MayureshMadavD <mayuresh.madav@gokwik.co>
2025-06-10 20:31:07 +0530

    ss

--- public/script.js
+++ public/script.js
@@ -9,12 +9,16 @@ function toggleStrikeThrough(checkbox) {
 
 const handleEditToogle = (event, i) => {
   event.preventDefault();
+  const isMobile = window.matchMedia("(max-width: 720px)").matches;
   const listOfTodo = document.querySelectorAll("li #edit_form");
   listOfTodo.forEach((list) => {
-    list.classList.add("hide");
+    list.classList.add("hide");  
   });
   const targetedTodoList = listOfTodo[i];
   if (targetedTodoList) {
     targetedTodoList.classList.remove("hide");
+    if (isMobile) {
+      targetedTodoList.classList.display = "block";
+    }
   }
 };
